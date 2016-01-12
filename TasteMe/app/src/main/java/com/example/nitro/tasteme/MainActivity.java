88736569462131e7046784a6b33a5e53746de315
dimitarsd <dsd363978@yahoo.com>
2016-01-13 01:04:10 +0200

    Implemented sign in and sign up of users. Validation of data. Added notifications of success/error Implemented Connection API - check for internet connection. Show error notification once and show progress bar till internet is enable Added App.java - used for initializing Parse

--- TasteMe/app/src/main/java/com/example/nitro/tasteme/MainActivity.java
+++ TasteMe/app/src/main/java/com/example/nitro/tasteme/MainActivity.java
@@ -48,8 +48,7 @@ public class MainActivity extends AppCompatActivity
 
         // [Optional] Power your app with Local Datastore. For more info, go to
         // https://parse.com/docs/android/guide#local-datastore
-        Parse.enableLocalDatastore(this);
-        Parse.initialize(this);
+
     }
 
     @Override
--- TasteMe/app/src/main/java/com/example/nitro/tasteme/RegisterActivity.java
+++ TasteMe/app/src/main/java/com/example/nitro/tasteme/RegisterActivity.java
@@ -1,10 +1,18 @@
 package com.example.nitro.tasteme;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class RegisterActivity  extends AppCompatActivity  {
     @Override
@@ -12,14 +20,58 @@ public class RegisterActivity  extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //Button register = (Button) findViewById(R.id.sign_up_button);
-        //register.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View v) {
-        //        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-        //        RegisterActivity.this.startActivity(intent);
-        //    }
-        //});
+        Button register = (Button) findViewById(R.id.registerButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText email = (EditText) findViewById(R.id.emailField);
+                EditText username = (EditText) findViewById(R.id.usernameField);
+                EditText password = (EditText) findViewById(R.id.passwordField);
+                EditText confirmPassword = (EditText) findViewById(R.id.confirmPasswordField);
+
+                if (!password.getText().toString().equals(confirmPassword.getText().toString())) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Passwords doesn't match";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
+                    toast.show();
+                    return;
+                }
+
+                ParseUser user = new ParseUser();
+
+                user.setUsername(email.getText().toString());
+                user.setPassword(password.getText().toString());
+                user.setEmail(email.getText().toString());
+                user.put("name", username.getText().toString());
+
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Context context = getApplicationContext();
+                            CharSequence text = "Sign up successful";
+                            int duration = Toast.LENGTH_SHORT;
+
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
+                            toast.show();
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Context context = getApplicationContext();
+                            CharSequence text = "Ops, cannot sign up right now";
+                            int duration = Toast.LENGTH_SHORT;
+
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
+                            toast.show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 }
