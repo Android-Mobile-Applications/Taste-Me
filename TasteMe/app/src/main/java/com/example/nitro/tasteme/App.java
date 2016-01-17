1c3478e054b439f6a5c3a1f53f49f830829d35a8
dimitarsd <dsd363978@yahoo.com>
2016-01-17 14:28:55 +0200

    Added click event on info window to show activity Made some changes of info window UI Changed directory of MapsActivity.java an UserInfoWindowsAdapter.java files

--- TasteMe/app/src/main/java/com/example/nitro/tasteme/App.java
+++ TasteMe/app/src/main/java/com/example/nitro/tasteme/App.java
@@ -4,11 +4,15 @@ import android.app.Application;
 
 import com.parse.Parse;
 
+// Define class for initializing Parse
 public class App extends Application {
+    private static final String applicationId = "8IuiYL4O1BYRWvDgSWV97l5zet2RxIrtwv9k8D3c";
+    private static final String clientId = "kYYqldFfSdoyoGEUYo81TDbzEibYfyrkywcipxS6";
+
     @Override
     public void onCreate() {
         super.onCreate();
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "8IuiYL4O1BYRWvDgSWV97l5zet2RxIrtwv9k8D3c", "kYYqldFfSdoyoGEUYo81TDbzEibYfyrkywcipxS6");
+        Parse.initialize(this, applicationId, clientId);
     }
 }
